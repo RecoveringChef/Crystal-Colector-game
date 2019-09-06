eee09468dc26f50fe9953bc4e7c24a30cfc00c6d
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-09-06 16:53:16 -0400

    more psuedocoding and tied in css stylesheet. verified by giving header background color

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+
+// variabel needed
+
+var = wins;
+var = losses;
+var = target;
+var = score;
+var = red;
+var = blue;
+var = purple;
+
+
+//generate target random number and output to html
+
+
+
+//generate random values for the 4 gems
+
+
+//function to add values of gems to score and output to html
+
+
+//if score  === target trigger wins incriment up and reset
+
+
+//else score > target  trigger loss incrimint up and reset
+
+
+// gems should be bootstrap cards
