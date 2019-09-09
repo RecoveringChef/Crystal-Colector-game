550eece3f6f96748037d3ea7cd316fb7e9624f98
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-09-09 01:19:19 -0400

    numbers are generating, css grid is laid out, func not yet written and noting going to screen -concole log only

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,33 @@
+//test hook up to js sheet
+console.log ("is this thing on?")
 
 // variabel needed
 
-var = wins;
-var = losses;
-var = target;
-var = score;
-var = red;
-var = blue;
-var = purple;
+var wins = 0;
+var losses = 0;
+var target = [Math.floor(Math.random() * (100 - 20) +20)];
+var score = 0;
+var red =  [Math.floor(Math.random() * (13 - 5) +5)];
+var blue =  [Math.floor(Math.random() * (6- 2) + 2)];
+var purple =  [Math.floor(Math.random() * (13 - 1) + 1)];
+var green =  [Math.floor(Math.random() * (3 - 1) + 1)];
+
+
+
+var winsText = document.getElementById("wins-text") + wins;
+var lossesText = document.getElementById("losses-text") + losses;
+var winsText = document.getElementById("target-text") + target;
+var lossesText = document.getElementById("score-text");
 
 
 //generate target random number and output to html
+//var target = target[Math.floor(Math.random() * (58 - 28 + 1))];
 
+ console.log("new target is: " + target) ;
+ console.log("new red: " + red) ;
+ console.log("new blue: " + blue);
+ console.log("new green: " + green) ;
+ console.log("new purple: " + purple) ;
 
 
 //generate random values for the 4 gems
